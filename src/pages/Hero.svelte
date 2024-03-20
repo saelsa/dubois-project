00a47a1804654c5abbfca28b430f53fc4538e2fb
saelsa <saelsa@gmx.net>
2024-03-20 08:09:41 +0100

    Initial commit

---
+++ src/pages/Hero.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Section from '../components/Section.svelte';
+</script>
+
+<Section id="hero">
+	<div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+		<h1
+			class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold"
+		>
+			Du Bois Challenge 2024.
+		</h1>
+		<h2 class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+			Weekly challenge to recreate data visualizations of the civil rights
+			activist Du Bois.
+		</h2>
+	</div>
+</Section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Section from '../components/Section.svelte';
+	import Hero from '../pages/Hero.svelte';
+</script>
+
+<main class="bg-[#faf0e6]">
+	<Hero />
+</main>
