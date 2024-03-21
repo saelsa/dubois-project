410a89f6a3701165a1b7eeb22d4f04c14f98db2d
saelsa <saelsa@gmx.net>
2024-03-21 06:15:13 +0100

    Create plate component

---
+++ src/pages/Green.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Plate from '../components/Plate.svelte';
+	import Section from '../components/Section.svelte';
+</script>
+
+<Section id="green">
+	<div class="flex flex-1 relative p-4">
+		<div class="absolute top-0 left-0 w-1/3 h-[1px] bg-[#008000] -translate-x-4"></div>
+		<div class="absolute top-0 left-0 h-2/3 w-[1px] bg-[#008000] -translate-y-4"></div>
+
+		<div class="grid grid-cols-1 gap-4 w-full"><Plate><h2>Hello</h2></Plate></div>
+	</div>
+</Section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Section from '../components/Section.svelte';
+	import Green from '../pages/Green.svelte';
 	import Hero from '../pages/Hero.svelte';
 </script>
 
 <main class="bg-[#faf0e6]">
 	<Hero />
+	<Green />
 </main>
